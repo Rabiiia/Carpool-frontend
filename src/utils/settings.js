b3d23664813a7afd27aa2ddf9e92b6b9fcfa596e
acmve <acmverden@hotmail.com>
2022-11-28 20:24:41 +0100

    create component

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -4,3 +4,6 @@ export const DROPLET_FOLDER = "/XXX" // <-- replace with your folder that your c
 export const BASE_URL = "http://localhost:8080" // <-- replace it with your droplet url so that you can use your database on your droplet instead of local database
 export const LOGIN_URL = BASE_URL + "/api/login"
 export const VERIFY_URL = BASE_URL + "/api/verify"
+
+//"https://acmverden.dk/tomcat/CA3"
+
